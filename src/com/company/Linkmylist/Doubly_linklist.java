960971288c95a2387b3096d25f50422d469fc8f5
tushar <tushar158gupta@gmail.com>
2022-09-22 23:28:20 +0530

    did doubly link list

--- src/com/company/Linkmylist/Doubly_linklist.java
+++ src/com/company/Linkmylist/Doubly_linklist.java
@@ -1,6 +1,6 @@
 package com.company.Linkmylist;
 
-class Node{
+ class Node{
     int data;
     Node prev;
     Node next;
@@ -12,8 +12,60 @@ class Node{
 }
 
 public class Doubly_linklist {
+    static Node addfront(Node head , int value){
+       Node  temp = new Node(value);
+        if(head == null) {
+          return temp;
+       }
+        else {
+            temp.next=head;
+            head.prev = temp;
+        }
+
+
+        return temp;
+    }
+
+    static Node remendlink(Node head){
+        if( head == null ){
+            return null;
+        }
+        if( head.next == null ){
+            return null;
+        }
+        Node curr = head;
+        while (curr.next!=null){
+            curr = curr.next;
+        }
+        curr = curr.prev;
+        curr.next= null;
+        return head;
+    }
+
+    static  void traverselink(Node head){
+        Node curr = head;
+        while(curr!=null){
+            System.out.println(curr.data);
+            curr=curr.next;
+        }
+
+    }
     public static void main(String[]arg){
         Node head = new Node(10);
-        System.out.println(head.prev);
+         head = addfront(head , 20);
+         head = addfront(head ,30);
+         head = addfront(head ,40);
+         head = addfront(head , 50);
+         head = addfront(head ,60);
+//        System.out.println(head.data);
+        head = addfront(head , 100);
+//        System.out.println(head.data);
+//        System.out.println(head.next.data);
+        remendlink(head);
+//        System.out.println(head.data);
+
+        traverselink(head);
+
+
     }
 }
